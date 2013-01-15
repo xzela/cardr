145efaf1f0e7962084d3144d68a4f21c123ff679
Zeph Lafassett <zeph@lafassett.com>
2013-01-15 14:49:52 -0800

    added code

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        THIS IS A FAKE SITE.
+        DO NOT BE FOOLED.
+        NO CREDIT CARD INFORMATION IS STORED.
+        ALL LOGS FOR THIS SITE ARE PURGED EVERY HOUR.
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        -->
+        <meta charset="utf-8">
+        <title>Online service to check if your credit card has been stolen</title>
+        <link href="http://fonts.googleapis.com/css?family=Rosario&v2" rel="stylesheet" type="text/css">
+        <link type="text/css" rel="stylesheet" href="style.css" />
+        <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <script type="text/javascript" src="jquery.min.js"></script>
+        <script type="text/javascript">
+            var value = 7;
+            var seconds = 3000;
+            $(document).ready(function() {
+                var url = '/check.php';
+                $('#submit').live('click',function() {
+                    $.post(url,{
+                        number: $('#credit').val(),
+                        name : $('#name').val()
+                    },
+                    function(res) {
+                        if(res.success) {
+                            $('#content').html(res.message);
+                        }
+                        else {
+                            $('.messages').html(res.message).slideDown('slow');
+                        }
+                    },
+                    'json');
+                });
+                var span = $('h2 span').html(value);
+                setInterval(function() {
+                    span.html(value++);
+                }, seconds);
+            });
+        </script>
+    </head>
+    <body>
+        <div id="content">
+            <header>
+              <h1><strong>Has your credit card number been stolen?</strong></h1>
+              <h2>Everyday, <span></span> million credit cards are skimmed or stolen.</h2>
+            </header>
+            <section>
+              <p><a href="http://cardr.org/" target="_blank">Cardr</a> monitors illegal number swapping hubs and alerts you of any concerns.</p>
+              <form method="POST" action="javascript:return false;">
+
+                <fieldset>
+                  <legend>Get started Now!</legend>
+                  <p>Submit your credit card number to our secure database</p>
+                  <section>
+                    <input id="credit" name="credit" type="creditcard" value="" placeholder="1234-5678-9123-4567" />
+                    <button id="submit" type="submit">Continue</button>
+                    <div class="messages"></div>
+                  </section>
+                </fieldset>
+              </form>
+            </section>
+            <section class="testimonials">
+                <h2><strong>Don't Believe Us? Read These Testimonials about Cardr!</strong></h2>
+                <div>
+                    <h3>"Cardr saves me from lots of trouble."</h3>
+                    <p class="testiment">
+                        Before using Cardr.org, I was always worried about my credit card numbers.
+                        I wouldn't use them to make purchases, online or off.
+                        But now that I do, <span>Cardr saves me from lots of trouble.</span>
+                    </p>
+                    <p class="pointer">&nbsp;</p>
+                    <p class="person">John Longfoots, Texas</p>
+                </div>
+                <div>
+                    <h3>Safety first!</h3>
+                    <p class="testiment"></p>
+                    <p class="pointer">&nbsp;</p>
+                    <p class='person'>Sue Stark, California</p>
+                </div>
+                <div>
+                    <h3>"For the love of God, use Cardr!"</h3>
+                    <p class="testiment">
+                        People have no idea how often their credit card numbers are stolen.
+                        You could be walking to work one day and then <strong>BLAM!</strong> Your credit card number is gone, just like that!
+                        I always tell people <span>for the love of God use Cardr!</span>
+                    </p>
+                    <p class="pointer">&nbsp;</p>
+                    <p class='person'>Joe Pritchards, Nevada</p>
+                </div>
+                <div>
+                    <h3>"Without Cardr, I'd be out on the streets!"</h3>
+                    <p class="testiment"><span>Without Cardr, I'd be out on the streets!</span></p>
+                    <p class="pointer">&nbsp;</p>
+                    <p class='person'>Sally Bachmann, New York</p>
+                </div>
+            </section>
+            <div class='footer'>
+                <h3>Cardr.org &copy; <?php echo date('Y'); ?></h3>
+            </div>
+        </div>
+    </body>
+</html>

e92144f1df45f131aac0c1e1244d4666cfff9e76
Chris <chris@contolini.com>
2013-01-16 22:08:31 -0500

    organized assets, added grunt

--- index.php
+++ index.php
@@ -11,44 +11,18 @@
         -->
         <meta charset="utf-8">
         <title>Online service to check if your credit card has been stolen</title>
-        <link href="http://fonts.googleapis.com/css?family=Rosario&v2" rel="stylesheet" type="text/css">
-        <link type="text/css" rel="stylesheet" href="style.css" />
+        <link href='http://fonts.googleapis.com/css?family=Crete+Round:400,400italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Miniver' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="assets/css/cardr.min.css?1358392093619" />
         <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-        <script type="text/javascript" src="jquery.min.js"></script>
-        <script type="text/javascript">
-            var value = 7;
-            var seconds = 3000;
-            $(document).ready(function() {
-                var url = '/check.php';
-                $('#submit').live('click',function() {
-                    $.post(url,{
-                        number: $('#credit').val(),
-                        name : $('#name').val()
-                    },
-                    function(res) {
-                        if(res.success) {
-                            $('#content').html(res.message);
-                        }
-                        else {
-                            $('.messages').html(res.message).slideDown('slow');
-                        }
-                    },
-                    'json');
-                });
-                var span = $('h2 span').html(value);
-                setInterval(function() {
-                    span.html(value++);
-                }, seconds);
-            });
-        </script>
     </head>
     <body>
         <div id="content">
             <header>
               <h1><strong>Has your credit card number been stolen?</strong></h1>
-              <h2>Everyday, <span></span> million credit cards are skimmed or stolen.</h2>
+              <h2>Every day, <span>23</span> million credit cards are skimmed or stolen.</h2>
             </header>
             <section>
               <p><a href="http://cardr.org/" target="_blank">Cardr</a> monitors illegal number swapping hubs and alerts you of any concerns.</p>
@@ -104,5 +78,6 @@
                 <h3>Cardr.org &copy; <?php echo date('Y'); ?></h3>
             </div>
         </div>
+        <script type="text/javascript" src="assets/js/cardr.min.js?1358392093619"></script>
     </body>
 </html>

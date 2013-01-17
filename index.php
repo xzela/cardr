ab47903a15d6203b902ee4c6ceda772215e5b352
Chris <chris@contolini.com>
2013-01-16 23:54:24 -0500

    rebuilding credit card form

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <title>Online service to check if your credit card has been stolen</title>
         <link href='http://fonts.googleapis.com/css?family=Crete+Round:400,400italic' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Miniver' rel='stylesheet' type='text/css'>
-        <link type="text/css" rel="stylesheet" href="assets/css/cardr.min.css?1358393487801" />
+        <link type="text/css" rel="stylesheet" href="assets/css/cardr.min.css?1358398270506" />
         <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
@@ -32,9 +32,11 @@
                   <legend>Get started Now!</legend>
                   <p>Submit your credit card number to our secure database</p>
                   <section>
-                    <input id="credit" name="credit" type="creditcard" value="" placeholder="1234-5678-9123-4567" />
-                    <button id="submit" type="submit">Continue</button>
-                    <div class="messages"></div>
+                    <form class="form-inline">
+                      <input id="credit" type="text" placeholder="1234-5678-9123-4567" >
+                      <button type="submit" class="btn">Continue</button>
+                      <div class="messages"></div>
+                    </form>
                   </section>
                 </fieldset>
               </form>
@@ -42,34 +44,25 @@
             <section class="testimonials">
                 <h2><strong>Don't Believe Us? Read These Testimonials about Cardr!</strong></h2>
                 <div>
-                    <h3>"Cardr saves me from lots of trouble."</h3>
                     <p class="testiment">
                         Before using Cardr.org, I was always worried about my credit card numbers.
                         I wouldn't use them to make purchases, online or off.
-                        But now that I do, <span>Cardr saves me from lots of trouble.</span>
+                        But now that I do, <strong>Cardr saves me from lots of trouble.</strong>
                     </p>
                     <p class="pointer">&nbsp;</p>
                     <p class="person">John Longfoots, Texas</p>
                 </div>
                 <div>
-                    <h3>Safety first!</h3>
-                    <p class="testiment"></p>
-                    <p class="pointer">&nbsp;</p>
-                    <p class='person'>Sue Stark, California</p>
-                </div>
-                <div>
-                    <h3>"For the love of God, use Cardr!"</h3>
                     <p class="testiment">
                         People have no idea how often their credit card numbers are stolen.
                         You could be walking to work one day and then <strong>BLAM!</strong> Your credit card number is gone, just like that!
-                        I always tell people <span>for the love of God use Cardr!</span>
+                        I always tell people <strong>for the love of God use Cardr!</strong>
                     </p>
                     <p class="pointer">&nbsp;</p>
                     <p class='person'>Joe Pritchards, Nevada</p>
                 </div>
                 <div>
-                    <h3>"Without Cardr, I'd be out on the streets!"</h3>
-                    <p class="testiment"><span>Without Cardr, I'd be out on the streets!</span></p>
+                    <p class="testiment">Without Cardr, I'd be out on the streets!</p>
                     <p class="pointer">&nbsp;</p>
                     <p class='person'>Sally Bachmann, New York</p>
                 </div>
@@ -78,6 +71,6 @@
                 <h3>Cardr.org &copy; <?php echo date('Y'); ?></h3>
             </div>
         </div>
-        <script type="text/javascript" src="assets/js/cardr.min.js?1358393487801"></script>
+        <script type="text/javascript" src="assets/js/cardr.min.js?1358398270506"></script>
     </body>
 </html>

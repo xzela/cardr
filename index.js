dadde8975f4799c2cbe825f5217a0911bfe54232
xzela <zeph@lafassett.com>
2016-05-10 17:56:45 -0700

    adding routes and libs

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var server = require('./server');
+(function() {
+	'use strict';
 
-server.start();
+	var restify = require('restify'),
+		PORT = 3001;
+
+	var server = restify.createServer();
+	server.name = 'cardr';
+
+	server.pre(function (req, res, next) {
+		console.log(req.method, req.url);
+		return next();
+	});
+
+	require('./routes')(server);
+
+	server.get('/', function (req, res) {
+		console.log('got request');
+		return res.send({});
+	});
+
+	server.listen(PORT, function () {
+		console.log('%s listening at %s', server.name, server.url);
+	});
+})();
